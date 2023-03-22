46bdc6631a13ba80a49c1d0b24471a2e2ba6fe2d
mega-kai <1968974979lv@gmail.com>
2023-03-22 14:28:44 +0800

    owo

--- src/lib.rs
+++ src/lib.rs
@@ -34,14 +34,13 @@ use std::{
 const GENERATION_COMPTYPE: CompType = CompType::new::<Generation>();
 const SPARSE_INDEX_COMPTYPE: CompType = CompType::new::<SparseIndex>();
 
-//-----------------COMPONENT TYPE-----------------//
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct CompType {
-    pub(crate) type_id: TypeId,
-    pub(crate) layout: Layout,
+    type_id: TypeId,
+    layout: Layout,
 }
 impl CompType {
-    pub(crate) const fn new<C: 'static>() -> Self {
+    const fn new<C: 'static>() -> Self {
         Self {
             type_id: TypeId::of::<C>(),
             layout: Layout::new::<C>(),
@@ -58,7 +57,6 @@ pub trait Component: Clone + 'static {
     }
 }
 
-//-----------------TYPE ERASED POINTER-----------------//
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 // this is for content, not generation or sparse index
 pub struct Ptr {
@@ -67,7 +65,7 @@ pub struct Ptr {
     sparse_index: SparseIndex,
 }
 impl Ptr {
-    pub(crate) fn new(ptr: *mut u8, comp_type: CompType, sparse_index: SparseIndex) -> Self {
+    fn new(ptr: *mut u8, comp_type: CompType, sparse_index: SparseIndex) -> Self {
         Self {
             ptr,
             comp_type,
@@ -75,11 +73,11 @@ impl Ptr {
         }
     }
 
-    pub(crate) fn cast_value(self) -> Value {
+    fn cast_value(self) -> Value {
         Value::new(self.ptr, self.comp_type)
     }
 
-    pub(crate) unsafe fn cast<T: 'static>(&self) -> Result<&mut T, &'static str> {
+    unsafe fn cast<T: 'static>(&self) -> Result<&mut T, &'static str> {
         if CompType::new::<T>() != self.comp_type {
             return Err("type not matching");
         } else {
@@ -100,14 +98,13 @@ impl Ord for Ptr {
     }
 }
 
-//-----------------TYPE ERASED VALUE-----------------//
 #[derive(Clone)]
-pub(crate) struct Value {
-    pub(crate) ptr: *mut u8,
-    pub(crate) comp_type: CompType,
+struct Value {
+    ptr: *mut u8,
+    comp_type: CompType,
 }
 impl Value {
-    pub(crate) fn new(src_ptr: *mut u8, comp_type: CompType) -> Self {
+    fn new(src_ptr: *mut u8, comp_type: CompType) -> Self {
         unsafe {
             let ptr = alloc(comp_type.layout);
             std::ptr::copy(src_ptr, ptr, comp_type.layout.size());
@@ -115,7 +112,7 @@ impl Value {
         }
     }
 
-    pub(crate) unsafe fn cast<T: 'static + Clone>(self) -> Result<T, &'static str> {
+    unsafe fn cast<T: 'static + Clone>(self) -> Result<T, &'static str> {
         if self.comp_type != CompType::new::<T>() {
             return Err("type not matching");
         } else {
@@ -132,122 +129,22 @@ impl Drop for Value {
     }
 }
 
-//-----------------TYPE ERASED PTR COLUMN/ROW-----------------//
-// all with the same component type and different sparse index
-#[derive(Clone)]
-pub(crate) struct PtrColumn {
-    pub(crate) comp_type: CompType,
-    // sorted with sparse index
-    pub(crate) vec: Vec<Ptr>,
-}
-impl PtrColumn {
-    pub(crate) fn new_empty(comp_type: CompType) -> Self {
-        Self {
-            vec: vec![],
-            comp_type,
-        }
-    }
+#[derive(Debug, Clone, PartialEq, Eq, Copy, PartialOrd, Ord, Hash)]
+struct SparseIndex(usize);
 
-    pub(crate) fn push(&mut self, mut access: Vec<Ptr>) -> Result<(), &'static str> {
-        // right now it doesn't check if there's duplicate ptr sparse index
-        for each in access.iter() {
-            if each.comp_type != self.comp_type {
-                return Err("type not matching");
-            }
-        }
-        self.vec.append(&mut access);
-        self.vec.sort();
-        Ok(())
-    }
+#[derive(Debug, Clone, PartialEq, Eq, Copy, PartialOrd, Ord, Hash)]
+struct DenseIndex(usize);
 
-    pub(crate) fn remove<R: RangeBounds<usize>>(&mut self, range: R) -> Vec<Ptr> {
-        let result = self.vec.drain(range).collect();
-        self.vec.sort();
-        result
-    }
-
-    pub(crate) fn is_empty(&self) -> bool {
-        self.vec.is_empty()
-    }
-}
-
-impl<'a> IntoIterator for &'a PtrColumn {
-    type Item = &'a Ptr;
-
-    type IntoIter = std::slice::Iter<'a, Ptr>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.vec.iter()
-    }
-}
-impl<'a> IntoIterator for &'a mut PtrColumn {
-    type Item = &'a mut Ptr;
-
-    type IntoIter = std::slice::IterMut<'a, Ptr>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.vec.iter_mut()
-    }
-}
-
-// all with the same id and and must have diff types
-#[derive(Clone)]
-pub struct PtrRow {
-    pub(crate) sparse_index: SparseIndex,
-    // not sorted
-    pub(crate) data: HashMap<CompType, Ptr>,
-}
-impl PtrRow {
-    pub(crate) fn new_empty(sparse_index: SparseIndex) -> Self {
-        Self {
-            data: HashMap::new(),
-            sparse_index,
-        }
-    }
-
-    pub(crate) fn push(&mut self, mut access_vec: Vec<Ptr>) -> Result<(), &'static str> {
-        // not checking if comp type is being replaced
-        for each in access_vec.into_iter() {
-            if each.sparse_index != self.sparse_index {
-                return Err("sparse index not matching");
-            }
-            self.data.insert(each.comp_type, each);
-        }
-        Ok(())
-    }
-
-    pub(crate) fn get(&self, comp_type: CompType) -> Result<Ptr, &'static str> {
-        Ok(self
-            .data
-            .get(&comp_type)
-            .ok_or("type not present in this row")?
-            .clone())
-    }
-
-    pub(crate) fn is_empty(&self) -> bool {
-        self.data.is_empty()
-    }
-}
-
-//-----------------GENERATION-----------------//
 #[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
-pub(crate) struct Generation(usize);
+struct Generation(usize);
 impl Generation {
-    pub(crate) fn advance(&mut self) -> Self {
+    fn advance(&mut self) -> Self {
         self.0 += 1;
         *self
     }
 }
 
-//-----------------SPARSE/DENSE INDEX-----------------//
-#[derive(Debug, Clone, PartialEq, Eq, Copy, PartialOrd, Ord, Hash)]
-pub(crate) struct SparseIndex(usize);
-
-#[derive(Debug, Clone, PartialEq, Eq, Copy, PartialOrd, Ord, Hash)]
-pub(crate) struct DenseIndex(usize);
-
-//----------------BASIC DATA STRUCTURES------------------//
-pub(crate) struct TypeErasedVec {
+struct TypeErasedVec {
     ptr: *mut u8,
     len: usize,
     cap: usize,
@@ -291,7 +188,7 @@ impl Drop for TypeErasedVec {
     }
 }
 
-pub(crate) struct DenseVec {
+struct DenseVec {
     sparse_index_vec: TypeErasedVec,
     comp_vec: TypeErasedVec,
 }
@@ -323,7 +220,7 @@ impl DenseVec {
     }
 }
 
-pub(crate) struct SparseVec {
+struct SparseVec {
     dense_index_vec: TypeErasedVec,
     generation_vec: TypeErasedVec,
 }
@@ -347,8 +244,7 @@ impl SparseVec {
     }
 }
 
-//----------------SPARSE SET------------------//
-pub(crate) struct SparseSet {
+struct SparseSet {
     comp_type: CompType,
 
     dense_vec: DenseVec,
@@ -356,29 +252,25 @@ pub(crate) struct SparseSet {
 }
 
 impl SparseSet {
-    pub(crate) fn new(comp_type: CompType, size: usize) -> Self {
+    fn new(comp_type: CompType, size: usize) -> Self {
         todo!()
     }
 
     //-----------------MAIN API-----------------//
-    pub(crate) fn is_taken(&self, sparse_index: SparseIndex) -> bool {
+    fn is_taken(&self, sparse_index: SparseIndex) -> bool {
         todo!()
     }
 
-    pub(crate) fn insert(
-        &mut self,
-        sparse_index: SparseIndex,
-        ptrs: Ptr,
-    ) -> Result<Ptr, &'static str> {
+    fn insert(&mut self, sparse_index: SparseIndex, ptrs: Ptr) -> Result<Ptr, &'static str> {
         todo!()
     }
 
-    pub(crate) fn remove(&mut self, sparse_index: SparseIndex) -> Result<Value, &'static str> {
+    fn remove(&mut self, sparse_index: SparseIndex) -> Result<Value, &'static str> {
         todo!()
     }
 
     // shallow move
-    pub(crate) fn move_value(
+    fn move_value(
         &mut self,
         from_index: SparseIndex,
         to_index: SparseIndex,
@@ -386,16 +278,12 @@ impl SparseSet {
         todo!()
     }
 
-    pub(crate) fn replace(
-        &mut self,
-        sparse_index: SparseIndex,
-        ptrs: Ptr,
-    ) -> Result<Value, &'static str> {
+    fn replace(&mut self, sparse_index: SparseIndex, ptrs: Ptr) -> Result<Value, &'static str> {
         todo!()
     }
 
     // shallow swap of two valid cells
-    pub(crate) fn swap_within(
+    fn swap_within(
         &mut self,
         sparse_index1: SparseIndex,
         sparse_index2: SparseIndex,
@@ -403,25 +291,24 @@ impl SparseSet {
         todo!()
     }
 
-    pub(crate) fn get_cell(&self, sparse_index: SparseIndex) -> Result<Ptr, &'static str> {
+    fn get_cell(&self, sparse_index: SparseIndex) -> Result<Ptr, &'static str> {
         todo!()
     }
 
-    pub(crate) fn get_column(&self) -> PtrColumn {
+    fn get_column(&self) -> () {
         todo!()
     }
 }
 
 pub struct Table {
     table: HashMap<CompType, SparseSet>,
-    row_type_cache: HashMap<SparseIndex, PtrRow>,
+    row_type_cache: HashMap<SparseIndex, ()>,
     bottom_sparse_index: SparseIndex,
 }
 
-// TODO: incorporate all the query filter methods within the table api, making it a more proper table data structure
 // TODO: variadic component insertion, probably with tuple
 impl Table {
-    pub(crate) fn new() -> Self {
+    fn new() -> Self {
         Self {
             table: HashMap::new(),
             row_type_cache: HashMap::new(),
@@ -430,7 +317,7 @@ impl Table {
     }
 
     //-----------------COLUMN MANIPULATION-----------------//
-    pub(crate) fn new_column(&mut self, comp_type: CompType) -> &mut SparseSet {
+    fn new_column(&mut self, comp_type: CompType) -> &mut SparseSet {
         if self.try_column(comp_type).is_ok() {
             panic!("type cannot be init twice")
         }
@@ -438,32 +325,27 @@ impl Table {
         self.table.get_mut(&comp_type).unwrap()
     }
 
-    pub(crate) fn get_column(&mut self, comp_type: CompType) -> Result<PtrColumn, &'static str> {
-        Ok(self.try_column(comp_type)?.get_column())
+    fn get_column(&mut self, comp_type: CompType) -> Result<(), &'static str> {
+        // Ok(self.try_column(comp_type)?.get_column())
+        todo!()
     }
 
-    pub(crate) fn remove_column(&mut self, comp_type: CompType) -> Option<SparseSet> {
+    fn remove_column(&mut self, comp_type: CompType) -> Option<SparseSet> {
         self.table.remove(&comp_type)
     }
 
     //-----------------ROW MANIPULATION-----------------//
-    pub(crate) fn new_row(&mut self) -> SparseIndex {
+    fn new_row(&mut self) -> SparseIndex {
         let result = self.bottom_sparse_index;
-        self.row_type_cache.insert(
-            self.bottom_sparse_index,
-            PtrRow::new_empty(self.bottom_sparse_index),
-        );
+        // todo cache
         self.bottom_sparse_index.0 += 1;
         result
     }
 
-    pub(crate) fn get_row(&mut self, sparse_index: SparseIndex) -> Result<PtrRow, &'static str> {
+    fn get_row(&mut self, sparse_index: SparseIndex) -> Result<(), &'static str> {
         // since init row ensures the existence of all row cache
-        let cache = self
-            .row_type_cache
-            .get(&sparse_index)
-            .ok_or("index overflow")?;
-        Ok(cache.clone())
+        // todo cache
+        todo!()
     }
 
     //-----------------HELPERS-----------------//
@@ -484,7 +366,7 @@ impl Table {
     //-----------------CELL IO OPERATION-----------------//
 
     // if not column init, init it automatically
-    pub(crate) fn push_cell(
+    fn push_cell(
         &mut self,
         sparse_index: SparseIndex,
         // todo: maybe Value??
@@ -493,11 +375,11 @@ impl Table {
         todo!()
     }
 
-    pub(crate) fn pop_cell(&mut self, access: Ptr) -> Result<Value, &'static str> {
+    fn pop_cell(&mut self, access: Ptr) -> Result<Value, &'static str> {
         todo!()
     }
 
-    pub(crate) fn replace_cell(
+    fn replace_cell(
         &mut self,
         access: Ptr,
         // todo maybe value?
@@ -509,7 +391,7 @@ impl Table {
     //-----------------CELL OPERATION WITHIN TABLE-----------------//
 
     /// one valid cell move to an empty one, returns the new table cell access
-    pub(crate) fn move_cell_within(
+    fn move_cell_within(
         &mut self,
         from_key: Ptr,
         to_index: SparseIndex,
@@ -518,7 +400,7 @@ impl Table {
     }
 
     /// two valid cells, move one to another location, and pop that location
-    pub(crate) fn replace_cell_within(
+    fn replace_cell_within(
         &mut self,
         from_key: Ptr,
         to_key: Ptr,
@@ -527,21 +409,9 @@ impl Table {
     }
 
     /// shallow swap between two valid cells
-    pub(crate) fn swap_cell_within(
-        &mut self,
-        key1: Ptr,
-        key2: Ptr,
-    ) -> Result<(Ptr, Ptr), &'static str> {
+    fn swap_cell_within(&mut self, key1: Ptr, key2: Ptr) -> Result<(Ptr, Ptr), &'static str> {
         todo!()
     }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-#[non_exhaustive]
-pub enum ExecutionFrequency {
-    Always,
-    Once(bool),
-    // Timed(f64, f64),
 }
 
 pub struct Command<'a> {
@@ -550,7 +420,7 @@ pub struct Command<'a> {
 
 // TODO: turns the api into wrapper functions of those in impl ComponentTable
 impl<'a> Command<'a> {
-    pub(crate) fn new(table: &'a mut Table) -> Self {
+    fn new(table: &'a mut Table) -> Self {
         Self { table }
     }
 
@@ -576,10 +446,17 @@ impl<'a> Command<'a> {
     // }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+#[non_exhaustive]
+pub enum ExecutionFrequency {
+    Always,
+    Once(bool),
+    // Timed(f64, f64),
+}
 pub struct System {
-    pub(crate) order: usize,
-    pub(crate) frequency: ExecutionFrequency,
-    pub(crate) func: fn(Command),
+    order: usize,
+    frequency: ExecutionFrequency,
+    func: fn(Command),
 }
 impl System {
     pub fn default(func: fn(Command)) -> Self {
@@ -598,7 +475,7 @@ impl System {
         }
     }
 
-    pub(crate) fn run(&self, table: &mut Table) {
+    fn run(&self, table: &mut Table) {
         (self.func)(Command::new(table))
     }
 
@@ -631,7 +508,7 @@ impl Ord for System {
 pub struct Scheduler {
     new_pool: Vec<System>,
     // waiting: Vec<System>,
-    pub(crate) queue: Vec<System>,
+    queue: Vec<System>,
 }
 impl Scheduler {
     pub fn new() -> Self {
@@ -646,7 +523,7 @@ impl Scheduler {
         self.new_pool.push(system);
     }
 
-    pub(crate) fn prepare_queue(&mut self) {
+    fn prepare_queue(&mut self) {
         self.queue.retain(|x| !x.is_once_run());
         if !self.new_pool.is_empty() {
             self.queue.append(&mut self.new_pool);
